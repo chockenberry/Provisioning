82afe5359252d1c115e6205a9988b3e0e996ca01
Craig Hockenberry <craig.hockenberry@gmail.com>
2013-10-29 12:50:44 -0700

    Simplified plain-text output by traversing the property list. Added summary for developer certificates included in the provisioning profile.

--- Provisioning/GeneratePreviewForURL.m
+++ Provisioning/GeneratePreviewForURL.m
@@ -15,13 +15,75 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
    This function's job is to create preview for designated file
    ----------------------------------------------------------------------------- */
 
+void displayKeyAndValue(NSUInteger level, NSString *key, id value, NSMutableString *output)
+{
+	int indent = (int)(level * 4);
+	
+	if ([value isKindOfClass:[NSDictionary class]]) {
+		if (key) {
+			[output appendFormat:@"%*s%@ = {\n", indent, "", key];
+		}
+		else {
+			[output appendFormat:@"%*s{\n", indent, ""];
+		}
+		NSDictionary *dictionary = (NSDictionary *)value;
+		NSArray *keys = [[dictionary allKeys] sortedArrayUsingSelector:@selector(compare:)];
+		for (NSString *key in keys) {
+			displayKeyAndValue(level + 1, key, [dictionary valueForKey:key], output);
+		}
+		[output appendFormat:@"%*s}\n", indent, ""];
+	}
+	else if ([value isKindOfClass:[NSArray class]]) {
+		[output appendFormat:@"%*s%@ = [\n", indent, "", key];
+		NSArray *array = (NSArray *)value;
+		for (id value in array) {
+			displayKeyAndValue(level + 1, nil, value, output);
+		}
+		[output appendFormat:@"%*s]\n", indent, ""];
+	}
+	else if ([value isKindOfClass:[NSData class]]) {
+		NSData *data = (NSData *)value;
+
+		// try to display the data as a certificate
+		SecCertificateRef certificateRef = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)data);
+		if (certificateRef) {
+			CFStringRef summaryRef = SecCertificateCopySubjectSummary(certificateRef);
+			if (key) {
+				[output appendFormat:@"%*s%@ = %@\n", indent, "", key, (__bridge NSString *)summaryRef];
+			}
+			else {
+				[output appendFormat:@"%*s%@\n", indent, "", (__bridge NSString *)summaryRef];
+			}
+			
+			CFRelease(summaryRef);
+			CFRelease(certificateRef);
+		}
+		else {
+			if (key) {
+				[output appendFormat:@"%*s%@ = %zd bytes of data\n", indent, "", key, [data length]];
+			}
+			else {
+				[output appendFormat:@"%*s%zd bytes of data\n", indent, "", [data length]];
+			}
+		}
+	}
+	else {
+		if (key) {
+			[output appendFormat:@"%*s%@ = %@\n", indent, "", key, value];
+		}
+		else {
+			[output appendFormat:@"%*s%@\n", indent, "", value];
+		}
+	}
+}
+
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
     @autoreleasepool {
 		
         // Load the property list from the URL
         NSURL *URL = (__bridge NSURL *)url;
-		NSLog(@"%s URL = %@", __PRETTY_FUNCTION__, URL);
+		NSLog(@"URL = %@", URL);
 
 		CMSDecoderRef decoder = NULL;
 		CMSDecoderCreate(&decoder);
@@ -32,10 +94,9 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
 			CMSDecoderFinalizeMessage(decoder);
 			CFDataRef dataRef = NULL;
 			CMSDecoderCopyContent(decoder, &dataRef);
+			NSData *data = (NSData *)CFBridgingRelease(dataRef);
 			
-			if (dataRef) {
-				NSString *fileContents = [[NSString alloc] initWithData:(NSData *)CFBridgingRelease(dataRef) encoding:NSUTF8StringEncoding];
-		
+			if (data) {
 				// check if the request was cancelled
 				if (! QLPreviewRequestIsCancelled(preview)) {
 #if 0
@@ -50,7 +111,12 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
 					QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding], kUTTypeHTML, (__bridge CFDictionaryRef)properties);
 #else
 					// for now, just use a plain-text representation
-					QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)[fileContents dataUsingEncoding:NSUTF8StringEncoding], kUTTypePlainText, NULL);
+					NSDictionary *propertyList = [NSPropertyListSerialization propertyListWithData:data options:0 format:NULL error:NULL];
+					if (propertyList) {
+						NSMutableString *output = [NSMutableString string];
+						displayKeyAndValue(0, nil, propertyList, output);
+						QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)[output dataUsingEncoding:NSUTF8StringEncoding], kUTTypePlainText, NULL);
+					}
 #endif
 				}
 			}

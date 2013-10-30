ad60b64b94d4c223a2679cc5ed2742bf81d03e6f
Craig Hockenberry <craig.hockenberry@gmail.com>
2013-10-29 18:08:57 -0700

    Information is now formatted in HTML. Still some work to do with summarizing creation and expiration dates.

--- Provisioning/GeneratePreviewForURL.m
+++ Provisioning/GeneratePreviewForURL.m
@@ -34,7 +34,7 @@ void displayKeyAndValue(NSUInteger level, NSString *key, id value, NSMutableStri
 		[output appendFormat:@"%*s}\n", indent, ""];
 	}
 	else if ([value isKindOfClass:[NSArray class]]) {
-		[output appendFormat:@"%*s%@ = [\n", indent, "", key];
+		[output appendFormat:@"%*s%@ = (\n", indent, "", key];
 		NSArray *array = (NSArray *)value;
 
 		// special case for provisioned devices: sort them so it's easier to find matches visually
@@ -45,7 +45,7 @@ void displayKeyAndValue(NSUInteger level, NSString *key, id value, NSMutableStri
 		for (id value in array) {
 			displayKeyAndValue(level + 1, nil, value, output);
 		}
-		[output appendFormat:@"%*s]\n", indent, ""];
+		[output appendFormat:@"%*s)\n", indent, ""];
 	}
 	else if ([value isKindOfClass:[NSData class]]) {
 		NSData *data = (NSData *)value;
@@ -105,10 +105,91 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
 			if (data) {
 				// check if the request was cancelled
 				if (! QLPreviewRequestIsCancelled(preview)) {
-#if 0
+#if 1
 					// eventually, the fileContents will be formatted using an HTML template...
 					NSURL *htmlURL = [[NSBundle bundleWithIdentifier:@"com.iconfactory.Provisioning"] URLForResource:@"template" withExtension:@"html"];
-					NSString *html = [NSString stringWithContentsOfURL:htmlURL encoding:NSUTF8StringEncoding error:NULL];
+					NSMutableString *html = [NSMutableString stringWithContentsOfURL:htmlURL encoding:NSUTF8StringEncoding error:NULL];
+
+					// use all keys and values in the property list to generate replacement tokens and values
+					NSDictionary *propertyList = [NSPropertyListSerialization propertyListWithData:data options:0 format:NULL error:NULL];
+					for (NSString *key in [propertyList allKeys]) {
+						NSString *replacementValue = [[propertyList valueForKey:key] description];
+						NSString *replacementToken = [NSString stringWithFormat:@"__%@__", key];
+						[html replaceOccurrencesOfString:replacementToken withString:replacementValue options:0 range:NSMakeRange(0, [html length])];
+					}
+					
+					// synthesize other replacement tokens and values
+					NSMutableDictionary *synthesizedInfo = [NSMutableDictionary dictionary];
+					id value = nil;
+					NSString *synthesizedValue = nil;
+					
+					value = [propertyList objectForKey:@"ApplicationIdentifierPrefix"];
+					if ([value isKindOfClass:[NSArray class]]) {
+						NSArray *array = (NSArray *)value;
+						synthesizedValue = [array componentsJoinedByString:@", "];
+						[synthesizedInfo setObject:synthesizedValue forKey:@"AppIds"];
+
+					}
+
+					value = [propertyList objectForKey:@"TeamIdentifier"];
+					if ([value isKindOfClass:[NSArray class]]) {
+						NSArray *array = (NSArray *)value;
+						synthesizedValue = [array componentsJoinedByString:@", "];
+						[synthesizedInfo setObject:synthesizedValue forKey:@"TeamIds"];
+					}
+
+					value = [propertyList objectForKey:@"Entitlements"];
+					if ([value isKindOfClass:[NSDictionary class]]) {
+						NSDictionary *dictionary = (NSDictionary *)value;
+						NSMutableString *synthesizedValue = [NSMutableString string];
+						displayKeyAndValue(0, nil, dictionary, synthesizedValue);
+						
+						[synthesizedInfo setObject:synthesizedValue forKey:@"EntitlementsFormatted"];
+					}
+					else {
+						[synthesizedInfo setObject:@"No Entitlements" forKey:@"EntitlementsFormatted"];
+					}
+
+					value = [propertyList objectForKey:@"DeveloperCertificates"];
+					if ([value isKindOfClass:[NSArray class]]) {
+						NSMutableArray *summaries = [NSMutableArray array];
+						NSArray *array = (NSArray *)value;
+						for (NSData *data in array) {
+							SecCertificateRef certificateRef = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)data);
+							if (certificateRef) {
+								CFStringRef summaryRef = SecCertificateCopySubjectSummary(certificateRef);
+								NSString *summary = (NSString *)CFBridgingRelease(summaryRef);
+								[summaries addObject:summary];
+								CFRelease(certificateRef);
+							}
+						}
+						synthesizedValue = [summaries componentsJoinedByString:@"<br/>"];
+						[synthesizedInfo setObject:synthesizedValue forKey:@"DeveloperCertificatesFormatted"];
+					}
+					else {
+						[synthesizedInfo setObject:@"No Developer Certificates" forKey:@"DeveloperCertificatesFormatted"];
+					}
+
+					value = [propertyList objectForKey:@"ProvisionedDevices"];
+					if ([value isKindOfClass:[NSArray class]]) {
+						NSArray *array = (NSArray *)value;
+						NSArray *sortedArray = [array sortedArrayUsingSelector:@selector(compare:)];
+						synthesizedValue = [sortedArray componentsJoinedByString:@"\n"];
+						[synthesizedInfo setObject:synthesizedValue forKey:@"ProvisionedDevicesFormatted"];
+						
+						synthesizedValue = [NSString stringWithFormat:@"%zd Devices", [array count]];
+						[synthesizedInfo setObject:synthesizedValue forKey:@"ProvisionedDevicesCount"];
+					}
+					else {
+						[synthesizedInfo setObject:@"" forKey:@"ProvisionedDevicesFormatted"];
+						[synthesizedInfo setObject:@"No Devices" forKey:@"ProvisionedDevicesCount"];
+					}
+
+					for (NSString *key in [synthesizedInfo allKeys]) {
+						NSString *replacementValue = [synthesizedInfo objectForKey:key];
+						NSString *replacementToken = [NSString stringWithFormat:@"__%@__", key];
+						[html replaceOccurrencesOfString:replacementToken withString:replacementValue options:0 range:NSMakeRange(0, [html length])];
+					}
 					
 					NSDictionary *properties = @{ // properties for the HTML data
 						(__bridge NSString *)kQLPreviewPropertyTextEncodingNameKey : @"UTF-8",
